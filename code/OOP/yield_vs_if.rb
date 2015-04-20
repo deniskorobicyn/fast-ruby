678d506d024b81d4af1bca92371c9b696fb49ebe
Denis <denis@devvela.com>
2015-04-20 14:42:12 +0500

    Added folder OOP versus straight code

---
+++ code/OOP/yield_vs_if.rb
@@ -0,0 +1,59 @@
+require 'benchmark/ips'
+class Object
+  def if_true
+    yield
+    self
+  end
+
+  def if_false
+    self
+  end
+end
+
+class NilClass
+  def if_true
+    self
+  end
+
+  def if_false
+    yield
+    self
+  end
+end
+
+class FalseClass
+  def if_true
+    self
+  end
+
+  def if_false
+    yield
+    self
+  end
+end
+
+ARRAY = (1..100).to_a.reverse
+
+def slow
+  ARRAY.each do |elem|
+    (elem % 2 == 0).
+      if_true{ 'even' }.
+      if_false{ 'odd' }
+  end
+end
+
+def fast
+  ARRAY.each do |elem|
+    if elem % 2 == 0
+      'even'
+    else
+      'odd'
+    end
+  end
+end
+
+Benchmark.ips do |x|
+  x.report('fast code description') { fast }
+  x.report('slow code description') { slow }
+  x.compare!
+end
